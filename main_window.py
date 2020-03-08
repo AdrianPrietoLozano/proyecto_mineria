b04254701cb0a8346a4613d903f091b6b159ba31
Adrian <prietolozano.adrian@gmail.com>
2020-03-08 05:54:01 -0600

    Se dejo solo un combo box para los atributos

--- main_window.py
+++ main_window.py
@@ -1,12 +1,16 @@
 from main_window_ui import *
 from dialogo_elegir_propiedades import *
 from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem, QMessageBox
-from PyQt5.QtCore import Qt, QDir, QItemSelectionModel
+from PyQt5.QtCore import Qt, QDir, QItemSelectionModel, QSize
+from PyQt5.QtGui import QIcon
 import pandas as pd
 from conjunto_datos import ConjuntoDatos
 from table_model_pandas import TableModelPandas
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    NUMERICO = 1
+    CATEGORICO = 2
+
     def __init__(self, ruta, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
@@ -18,17 +22,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.model = TableModelPandas(self.conjunto.panda)
         self.tabla.setModel(self.model)        
 
-        self.llenar_combo_boxes()
-        self.mostrar_atributo_numerico() # muestra los datos del atributo numerico seleccionado por defecto
-        self.mostrar_atributo_categorico() # muestra los datos del atributo categorico seleccionado por defecto
+        self.llenar_combo_box()
+        self.mostrar_atributo() # muestra los datos del atributo seleccionado por defecto
 
         #eventos cuando se cambia de elemento en el combo box
-        self.comboBoxNumericos.currentIndexChanged.connect(lambda x: self.mostrar_atributo_numerico())
-        self.comboBoxCategoricos.currentIndexChanged.connect(lambda x: self.mostrar_atributo_categorico())
+        self.comboBoxAtributos.currentIndexChanged.connect(lambda x: self.mostrar_atributo())
 
         #eventos botones actualizar atributo
-        self.btnActualizarNumericos.clicked.connect(self.actualizar_atributo_numerico)
-        self.btnActualizarCategoricos.clicked.connect(self.actualizar_atributo_categorico)
+        self.btnActualizar.clicked.connect(self.actualizar_atributo)
+
+        self.comboBoxAtributos.setIconSize(QSize(12, 12))
 
         # muestrar la información general del conjunto de datos
         self.labelNumInstancias.setText(str(self.conjunto.getNumInstancias()))
@@ -46,133 +49,85 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for i in range(len(row)):
                 self.tabla.setItem(index, i, QTableWidgetItem(str(row[i])))
 
-    def llenar_combo_boxes(self):
+    def llenar_combo_box(self):
         """Llena los combo box con los nombres de los atributos separados por tipo"""
         for atributo in self.conjunto.getAtributos():
             if atributo.getTipo() == "numerico":
-                self.comboBoxNumericos.addItem(atributo.getNombre())
+                self.comboBoxAtributos.addItem(QIcon("iconos/numerico.ico"), atributo.getNombre(), userData=self.NUMERICO)
             elif atributo.getTipo() == "categorico":
-                self.comboBoxCategoricos.addItem(atributo.getNombre())
+                self.comboBoxAtributos.addItem(QIcon("iconos/categorico.ico"), atributo.getNombre(), userData=self.CATEGORICO)
 
-    def actualizar_label_fuera_dominio(self, label, atributo):
+    def actualizar_label_fuera_dominio(self, atributo):
         fuera_dominio = len(atributo.getValoresFueraDominio())
         total = self.conjunto.getNumInstancias()
         porcentaje = (fuera_dominio * 100) / total
         texto = str(fuera_dominio) + " (" + str(round(porcentaje, 2)) +"%)"
-        label.setText(texto)
+        self.labelFueraDominio.setText(texto)
 
-    def actualizar_label_valores_faltantes(self, label, atributo):
+    def actualizar_label_valores_faltantes(self, atributo):
         faltantes = len(atributo.getValoresFaltantes())
         total = self.conjunto.getNumInstancias()
         porcentaje = (faltantes * 100) / total
         texto = str(faltantes) + " (" + str(round(porcentaje, 2)) +"%)"
-        label.setText(texto)
+        self.labelValoresFaltantes.setText(texto)
 
-    def mostrar_atributo_numerico(self):
-        """"Muestra los datos del atributo numerico actual que esta en el combo box"""
-        if self.comboBoxNumericos.count() == 0: # si no hay elementos en el combo box se desactiva
-            self.nombreAtributoNumerico.setText("")
-            self.tipoAtributoNumerico.setText("")
-            self.dominioAtributoNumerico.setText("")
-            self.groupBoxNumericos.setEnabled(False)
+    def mostrar_atributo(self):
+        """"Muestra los datos del atributo actual que esta en el combo box"""
+        if self.comboBoxAtributos.count() == 0: # si no hay elementos en el combo box se desactiva
+            self.nombreAtributo.setText("")
+            self.tipoAtributo.setText("")
+            self.dominioAtributo.setText("")
+            self.groupBoxAtributos.setEnabled(False)
             #falta limpiar mas etiquetas
         
         else:
-            if not self.groupBoxNumericos.isEnabled():
-                self.groupBoxNumericos.setEnabled(True)
+            if not self.groupBoxAtributos.isEnabled():
+                self.groupBoxAtributos.setEnabled(True)
 
-            nombre_atributo = self.comboBoxNumericos.currentText()
+            nombre_atributo = self.comboBoxAtributos.currentText()
             atributo = self.conjunto.getAtributo(nombre_atributo)
-            self.nombreAtributoNumerico.setText(atributo.getNombre())
-            self.tipoAtributoNumerico.setText(atributo.getTipo())
-            self.dominioAtributoNumerico.setText(atributo.getDominio())
+            self.nombreAtributo.setText(atributo.getNombre())
+            self.tipoAtributo.setText(atributo.getTipo())
+            self.dominioAtributo.setText(atributo.getDominio())
 
-            self.actualizar_label_fuera_dominio(self.labelFueraDominioNumerico, atributo)
-            self.actualizar_label_valores_faltantes(self.labelValoresFaltantesNumericos, atributo)
+            self.actualizar_label_fuera_dominio(atributo)
+            self.actualizar_label_valores_faltantes(atributo)
 
     
-
-    def mostrar_atributo_categorico(self):
-        """Muestra los datos del atributo categorico actual que esta en el combo box"""
-        if self.comboBoxCategoricos.count() == 0: # si no hay elementos en el combo box se desactiva
-            self.nombreAtributoCate.setText("")
-            self.tipoAtributoCate.setText("")
-            self.dominioAtributoCate.setText("")
-            self.groupBoxCategoricos.setEnabled(False)
-            #falta limpiar mas etiquetas
-
-        else:
-            if not self.groupBoxCategoricos.isEnabled():
-                self.groupBoxCategoricos.setEnabled(True)
-
-            nombre_atributo = self.comboBoxCategoricos.currentText()
-            atributo = self.conjunto.getAtributo(nombre_atributo)
-            self.nombreAtributoCate.setText(atributo.getNombre())
-            self.tipoAtributoCate.setText(atributo.getTipo())
-            self.dominioAtributoCate.setText(atributo.getDominio())
-
-            self.actualizar_label_fuera_dominio(self.labelFueraDominioCategorico, atributo)
-            self.actualizar_label_valores_faltantes(self.labelValoresFaltantesCategoricos, atributo)
-            
-
-    def actualizar_atributo_numerico(self):
-        """Evento clic del boton para acualizar un atributo numéricos"""
-        nombre_atributo = self.comboBoxNumericos.currentText()
+    def actualizar_atributo(self):
+        """Evento clic del boton para acualizar un atributo"""
+        nombre_atributo = self.comboBoxAtributos.currentText()
         atributo = self.conjunto.getAtributo(nombre_atributo)
 
         # falta comprobar que los datos no esten vacios
-        nom = self.nombreAtributoNumerico.text()
-        tipo = self.tipoAtributoNumerico.text()
-        dominio = self.dominioAtributoNumerico.text()
-        index = self.comboBoxNumericos.currentIndex()
+        nom = self.nombreAtributo.text()
+        tipo = self.tipoAtributo.text()
+        dominio = self.dominioAtributo.text()
+        index = self.comboBoxAtributos.currentIndex()
 
         if nom != atributo.getNombre():
             atributo.setNombre(nom)
-            self.comboBoxNumericos.setItemText(index, nom)
+            self.comboBoxAtributos.setItemText(index, nom)
             #self.tabla.selectColumn(QItemSelectionModel.Deselect) # deselecciona las columnas seleccionadas
             #indexColumna = self.encontrar_index_columna(nom)
             #self.tabla.selectColumn(indexColumna)
 
         if tipo != atributo.getTipo():
+            atributo.setTipo(tipo)
             if tipo == "categorico":
-                atributo.setTipo(tipo)
-                self.comboBoxCategoricos.addItem(atributo.getNombre())
-                self.comboBoxNumericos.removeItem(index)
+                nuevo_tipo = self.CATEGORICO
+                icono = QIcon("iconos/categorico.ico")
+            else:
+                nuevo_tipo = self.NUMERICO
+                icono = QIcon("iconos/numerico.ico")
+
+            self.comboBoxAtributos.setItemData(index, nuevo_tipo)
+            self.comboBoxAtributos.setItemIcon(index, icono)
+                
 
         if dominio != atributo.getDominio():
             atributo.setDominio(dominio)
-            self.actualizar_label_fuera_dominio(self.labelFueraDominioNumerico, atributo)
-
-
-
-    def actualizar_atributo_categorico(self):
-        """Evento clic del boton para acualizar un atributo categorico"""
-        nombre_atributo = self.comboBoxCategoricos.currentText()
-        atributo = self.conjunto.getAtributo(nombre_atributo)
-
-        # falta comprobar que los datos no esten vacios
-        nom = self.nombreAtributoCate.text()
-        tipo = self.tipoAtributoCate.text()
-        dominio = self.dominioAtributoCate.text()
-        index = self.comboBoxCategoricos.currentIndex()
-
-        if nom != atributo.getNombre():
-            atributo.setNombre(nom)
-            self.comboBoxCategoricos.setItemText(index, nom)
-            #self.tabla.selectColumn(QItemSelectionModel.Deselect) # deselecciona las columnas seleccionadas
-            #indexColumna = self.encontrar_index_columna(nom)
-            #self.tabla.selectColumn(indexColumna)
-
-
-        if tipo != atributo.getTipo():
-            if tipo == "numerico":
-                atributo.setTipo(tipo)
-                self.comboBoxNumericos.addItem(atributo.getNombre())
-                self.comboBoxCategoricos.removeItem(index)
-
-        if dominio != atributo.getDominio():
-            atributo.setDominio(dominio)
-            self.actualizar_label_fuera_dominio(self.labelFueraDominioCategorico, atributo)
+            self.actualizar_label_fuera_dominio(self.labelFueraDominio, atributo)
 
 
 
